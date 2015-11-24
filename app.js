7058d2b6d126366d1ab84bde1b099bacdb42328e
Paul Beardsell <paul@carterdigital.com.au>
2015-11-24 19:56:06 +1100

    Initial Commit

---
+++ app.js
@@ -0,0 +1,13 @@
+var http = require("http"),
+    express = require("express");
+
+
+var app = express();
+
+require('./routes')(app);
+
+var server = app.listen(3000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('Example app listening at http://%s:%s', host, port);
+});
